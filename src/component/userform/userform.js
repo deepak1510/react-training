f15a9aaf528ea5084fb3720561ce3fc2c56e06fe
Deepak Gupta <deepak.gupta@pinelabs.com>
2021-01-11 09:49:30 +0530

    'Post'

--- src/component/userform/userform.js
+++ src/component/userform/userform.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BackendService } from '../../backend-service';
 import "./userform.css"
 
 // export function Userform(props){
@@ -14,51 +15,67 @@ export class Userform extends React.Component{
         this.state ={
             user :{
                     fname : 'Deepak',
-                    age :30
+                    age :30,
+                    salary:200
                 },
             users :[]
         }
     }
     save = (event) =>{
      //   this.state.users.push(this.state.user)
-        this.setState({
-            users:[...this.state.users,this.state.user]
-        })
+        BackendService.saveUser(this.state.user, (response) =>{
+            this.setState({
+                users:[...this.state.users,Object.assign({},this.state.user)]
+            })
+        }).fail((error) =>{
+            alert("something went wrong, please retry");
+        });
+        
+    };
+    deleteUser = (index) =>{
+        console.log(this,index);
+        const dicision = window.confirm("Are you sure");
+        if(!dicision){
+            return;
+        }
+        this.state.users.splice(index,1)
+         this.setState({
+             users:this.state.users
+         })
     };
     onChangeHandleEvent = (event)=>{
         this.setState ({ //to rerender, call setState
-            user :{...this.state.user, [event.target.name] : event.target.value}
-         //   user :Object.assign(this.state.user, {[event.target.name] : event.target.value})
+           // user :{...this.state.user, [event.target.name] : event.target.value}
+            user :Object.assign(this.state.user, {[event.target.name] : event.target.value})
         });
     };
     render (){
+        const userModel = this.state.user;
         return (
             <div>
-                <input value={this.state.user.fname} name="fname" onChange={this.onChangeHandleEvent} 
+                <input value={userModel.fname} name="fname" onChange={this.onChangeHandleEvent} 
                     placeholder={this.props.lable}  style={{background:this.props.color}}></input>
                 <input value={this.state.user.age} name="age"
                     onChange={this.onChangeHandleEvent}                
-                placeholder="first name copy" style={{background:this.props.color}}></input>
-                
+                placeholder="Age" style={{background:this.props.color}}></input>
+                <input value={this.state.user.salary} name="salary"
+                    onChange={this.onChangeHandleEvent}                
+                placeholder="Salary" style={{background:this.props.color}}></input>
                 <button onClick={this.save}>Save</button>
+                
                 <table>
                     <thead>
-                        <th>
-                            First name
-                        </th>
-                        <th>
-                            Age
-                        </th>
+                        <th>First name</th>
+                        <th>Age</th>
+                        <th>Salary</th>
                     </thead>
                     <tbody>
-                       {this.state.users.map((user) =>{
+                       {this.state.users.map((user,index) =>{
                            return <tr>
-                               <td>
-                                   {user.fname}
-                               </td>
-                               <td>
-                                   {user.age}
-                               </td>
+                               <td>{user.fname}</td>
+                               <td>{user.age}</td>
+                               <td>{user.salary}</td>
+                               <td><button id={index} onClick={this.deleteUser.bind(this,index)}>Delete</button></td>
                            </tr>
                        })}
                     </tbody>
